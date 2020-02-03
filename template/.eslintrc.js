0ed72a1e2afa25f302ce2559bb5d7f7ecab7eb5a
Titouan Mathis <titouan@studiometa.fr>
2020-02-03 13:31:55 +0100

    Update the templates dependencies

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -2,33 +2,9 @@ const config = require('./build/config');
 
 module.exports = {
   root: true,
-  overrides: [
-    {
-      files: ['*.js'],
-      extends: '@studiometa/eslint-config/prettier-es6',
-    },
-    {
-      files: ['*.vue'],
-      extends: '@studiometa/eslint-config/vue',
-    },
-  ],
-  globals: {
-    window: false,
-    document: false,
-  },
-  parser: 'babel-eslint',
-  parserOptions: {
-    ecmaVersion: 2017,
-    sourceType: 'module',
-  },
-  env: {
-    es6: true,
-  },
+  extends: '@studiometa/eslint-config',
   settings: {
     'import/resolver': {
-      node: {
-        extensions: ['.js', '.mjs', '.vue'],
-      },
       alias: {
         map: Object.entries(config.alias),
         extensions: ['.js', '.mjs', '.json', '.vue', '.svg'],
