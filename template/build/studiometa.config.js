5e8fab2a65786f9b093cdbd767866a60d380f65e
Titouan Mathis <titouan@studiometa.fr>
2020-02-26 16:19:08 +0100

    Update the entry points list to match the current structure proposal

--- template/build/studiometa.config.js
+++ template/build/studiometa.config.js
@@ -1,7 +1,9 @@
 // Paths must be relative to the package.json root
 module.exports = {
   src: [
-    './web/wp-content/themes/<%= slug %>/src/scripts/**/[!_]*.js',
+    './web/wp-content/themes/<%= slug %>/src/scripts/app.js',
+    './web/wp-content/themes/<%= slug %>/src/scripts/pages/*.js',
+    './web/wp-content/themes/<%= slug %>/src/scripts/pages/**/index.js',
     './web/wp-content/themes/<%= slug %>/src/styles/**/[!_]*.scss',
   ],
   dist: './web/wp-content/themes/<%= slug %>/dist/',
