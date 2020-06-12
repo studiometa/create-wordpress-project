1c834467e7c74b09bbe9ad4b4f8b26eeec8996d1
Jean-Baptiste Bailly <jb@studiometa.fr>
2020-06-12 12:28:45 +0200

    Resolve conflicts and remove useless test for purgeCSS since tailwind handles them

--- template/tailwind.config.js
+++ template/tailwind.config.js
@@ -13,7 +13,6 @@ module.exports = merge(config, {
   // theme: {...},
   purge: {
     // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
-    enabled: process.env.NODE_ENV === 'production',
     content: [
       './web/wp-content/themes/<%= slug %>/src/js/**/*.js',
       './web/wp-content/themes/<%= slug %>/src/js/**/*.vue',
