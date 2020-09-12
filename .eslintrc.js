2a5cd030a8af012e0b231468508087a1390dfee9
Ludovic <ludovic@studiometa.fr>
2020-09-12 09:44:56 +0200

    Update .eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,12 @@
 module.exports = {
-	extends: '@studiometa/eslint-config/prettier-es6',
-	overrides: [
-		{
-			files: ['gulpfile.js', 'webpack.config.js'],
-			rules: {
-				'global-require': 'off',
-				'import/no-extraneous-dependencies': 'off',
-			},
-		},
-	]
+  extends: '@studiometa/eslint-config',
+  overrides: [
+    {
+      files: ['gulpfile.js', 'webpack.config.js'],
+      rules: {
+        'global-require': 'off',
+        'import/no-extraneous-dependencies': 'off',
+      },
+    },
+  ],
 };
