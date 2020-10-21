faced6debc290d579c4a3eeb50a5022173a49fcf
Ludovic <ludovic@studiometa.fr>
2020-10-21 15:18:48 +0200

    Update project dev dependencies
    - Update tailwind.config to use the new preset feature

--- template/tailwind.config.js
+++ template/tailwind.config.js
@@ -1,13 +1,11 @@
-const merge = require('lodash.merge');
-const config = require('@studiometa/tailwind-config');
-
 /**
  * TailwindCSS Configuration File
  *
  * @docs    https://tailwindcss.com/docs/configuration
  * @default https://github.com/studiometa/tailwind-config/blob/develop/src/index.js
  */
-module.exports = merge(config, {
+module.exports = {
+  presets: [require('tailwindcss/defaultConfig'), require('@studiometa/tailwind-config')],
   // Extends the default Studio Meta Tailwind configuration here...
   // plugins: [...],
   // theme: {...},
@@ -19,4 +17,4 @@ module.exports = merge(config, {
       './web/wp-content/themes/<%= slug %>/**/*.twig',
     ],
   },
-});
+};
