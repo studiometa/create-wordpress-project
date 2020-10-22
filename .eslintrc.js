440c122931bd5941c10ab5cafd2f34948ac57d02
Ludovic <ludovic@studiometa.fr>
2020-10-22 13:48:59 +0200

    Suppress eslintrc overrides

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,3 @@
 module.exports = {
   extends: '@studiometa/eslint-config',
-  overrides: [
-    {
-      files: ['gulpfile.js', 'webpack.config.js'],
-      rules: {
-        'global-require': 'off',
-        'import/no-extraneous-dependencies': 'off',
-      },
-    },
-  ],
 };
