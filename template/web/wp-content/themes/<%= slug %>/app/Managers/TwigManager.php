86fde3db3120f2c63bc51ef6fa7558ea62714d10
Ludovic <ludovic@studiometa.fr>
2020-11-16 14:44:37 +0100

    Add Twig debug comments
    - Add HTML comment before/after every Twig {% include %}, {% embed %}
    - Add HTML comment with current WordPress template above <html> tag
    - Fix #36

--- template/web/wp-content/themes/<%= slug %>/app/Managers/TwigManager.php
+++ template/web/wp-content/themes/<%= slug %>/app/Managers/TwigManager.php
@@ -11,11 +11,14 @@ use Studiometa\Managers\ManagerInterface;
 
 /** Class */
 class TwigManager implements ManagerInterface {
+	// phpcs:ignore Generic.Commenting.DocComment.MissingShort
 	/**
 	 * @inheritDoc
 	 */
 	public function run() {
 		add_filter( 'timber/twig', array( $this, 'add_twig_template_from_string' ) );
+		add_filter( 'timber/twig', array( $this, 'add_twig_template_include_comments' ) );
+		add_filter( 'timber/output', array( $this, 'add_twig_template_render_comments' ), 10, 3 );
 	}
 
 	/**
@@ -28,5 +31,37 @@ class TwigManager implements ManagerInterface {
 	public function add_twig_template_from_string( \Twig\Environment $twig ) {
 		$twig->addExtension( new \Twig\Extension\StringLoaderExtension() );
 		return $twig;
+	}
+
+	/**
+	 * Adds template_from_string to Twig.
+	 *
+	 * @link https://github.com/djboris88/twig-commented-include
+	 * @param \Twig\Environment $twig The Twig environment.
+	 * @return \Twig\Environment
+	 */
+	public function add_twig_template_include_comments( \Twig\Environment $twig ) {
+		if ( getenv( 'APP_DEBUG' ) === 'false' ) {
+			return $twig;
+		}
+
+		$twig->addExtension( new \Djboris88\Twig\Extension\CommentedIncludeExtension() );
+		return $twig;
+	}
+
+	/**
+	 * Add debug comments to Timber::render
+	 *
+	 * @param string $output HTML.
+	 * @param array  $data data.
+	 * @param string $file name.
+	 * @return string
+	 */
+	public function add_twig_template_render_comments( $output, $data, $file ) {
+		if ( getenv( 'APP_DEBUG' ) === 'false' ) {
+			return $output;
+		}
+
+		return "\n<!-- Begin output of '" . $file . "' -->\n" . $output . "\n<!-- / End output of '" . $file . "' -->\n";
 	}
 }
