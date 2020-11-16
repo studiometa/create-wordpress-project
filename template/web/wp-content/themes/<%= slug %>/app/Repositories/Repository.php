f1f4a59eac780ab2dc18f500efa7d1451809d051
Ludovic <ludovic@studiometa.fr>
2020-11-16 18:29:02 +0100

    Update Repository to be an abstract class

--- template/web/wp-content/themes/<%= slug %>/app/Repositories/Repository.php
+++ template/web/wp-content/themes/<%= slug %>/app/Repositories/Repository.php
@@ -9,10 +9,9 @@
 namespace Studiometa\Repositories;
 
 use Timber\PostCollection;
-use Timber\PostQuery;
 
 /** Class */
-class Repository {
+abstract class Repository {
 	/**
 	 * List of posts.
 	 *
@@ -65,33 +64,34 @@ class Repository {
 	/**
 	 * Runs a query.
 	 *
-	 * @param array  $params    WP_Query params.
-	 * @param string $post_class Post class to return.
+	 * @param array  $params     Query params.
+	 * @param string $class_type Post class to return.
 	 *
 	 * @return Repository
 	 */
-	protected function query( array $params, $post_class = '\Timber\Post' ) {
-
+	protected function query( array $params, $class_type ) {
 		// Clear old result sets.
 		$this->reset();
 
 		$cache_key    = __FUNCTION__ . md5( http_build_query( $params ) );
-		$cached_posts = wp_cache_get( $cache_key, __CLASS__ );
+		$cached_results = wp_cache_get( $cache_key, __CLASS__ );
 
-		if ( false !== $cached_posts && count( $cached_posts ) > 0 ) {
+		if ( false !== $cached_results && count( $cached_results ) > 0 ) {
 			// Use cached results.
-			return $this->result_set( $cached_posts );
+			return $this->result_set( $cached_results );
 		}
 
-		$posts = new PostQuery( $params, $post_class );
+		$results = $this->do_query( $params, $class_type );
 
 		// Cache our results.
-		if ( count( $posts ) > 0 ) {
-			wp_cache_set( $cache_key, $posts, __CLASS__ );
+		if ( count( $results ) > 0 ) {
+			wp_cache_set( $cache_key, $results, __CLASS__ );
 		}
 
-		return $this->result_set( $posts );
+		return $this->result_set( $results );
 	}
+
+	abstract function do_query( $params, $class_type );
 
 	/**
 	 * Clears the current result set.
