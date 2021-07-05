0e995d8ef166939680ca73b9a500774bc5df5e37
Lucas Simeon <lu.simeon@gmail.com>
2021-07-05 16:23:34 +0200

    Autoload studiometa-plugin-disabler plugin

---
+++ template/web/wp-content/mu-plugins/autoload.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Autoload MU plugins.
+ *
+ * @package    studiometa/create-wordpress-project
+ * @author     Studio Meta <agence@studiometa.fr>
+ * @copyright  2021 Studio Meta
+ * @license    https://opensource.org/licenses/MIT
+ * @since      1.0.0
+ * @version    1.0.0
+ */
+
+require WPMU_PLUGIN_DIR . '/studiometa-plugin-disabler/class-studiometaplugindisabler.php';
