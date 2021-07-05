7bccdf331f3431a2814fd380d86ae103b5656295
Lucas Simeon <lu.simeon@gmail.com>
2021-07-05 15:42:22 +0200

    Add a plugin disabler mu-plugin
    This mu-plugin disable plugins depends on environment when it's specified in `.env` file.

---
+++ template/web/wp-content/mu-plugins/studiometa-plugin-disabler/README.md
@@ -0,0 +1,10 @@
+# Studio Meta plugin disabler
+
+Disable plugins depends on environment.
+
+## Usage
+
+The following server environment variables must be set:
+```bash
+DISABLE_PLUGINS_[ENVIRONMENT]=example/example1.php
+```
---
+++ template/web/wp-content/mu-plugins/studiometa-plugin-disabler/class-studiometaplugindisabler.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Disable plugins depends on environment.
+ *
+ * @package    studiometa/create-wordpress-project
+ * @author     Studio Meta <agence@studiometa.fr>
+ * @copyright  2021 Studio Meta
+ * @license    https://opensource.org/licenses/MIT
+ * @since      1.0.0
+ * @version    1.0.0
+ */
+
+require_once __DIR__ . '/class-disableplugins.php';
+
+/**
+ * StudiometaPluginDisabler class.
+ */
+class StudiometaPluginDisabler {
+	/**
+	 * Constructor.
+	 */
+	public function __construct() {
+		$this->run();
+	}
+
+	/**
+	 * Disable plugins.
+	 *
+	 * @return void
+	 */
+	public function run() {
+		$env = getenv( 'WP_ENV' );
+
+		if ( ! $env ) {
+			return;
+		}
+
+		$plugins_to_disable_raw = getenv( 'DISABLE_PLUGINS_' . strtoupper( $env ) );
+
+		if ( ! $plugins_to_disable_raw ) {
+			return;
+		}
+
+		$plugins_to_disable = explode( ',', $plugins_to_disable_raw );
+
+		if ( empty( $plugins_to_disable ) || ! is_array( $plugins_to_disable ) ) {
+			return;
+		}
+
+		new DisablePlugins( $plugins_to_disable );
+	}
+}
+
+new StudiometaPluginDisabler();
