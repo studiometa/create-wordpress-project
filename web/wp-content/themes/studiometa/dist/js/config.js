b8246d819d2aecf56f1e4d81c584fa6593061c51
Titouan Mathis <titouan@studiometa.fr>
2022-06-15 16:46:25 +0200

    Build assets

--- web/wp-content/themes/studiometa/dist/js/config.js
+++ web/wp-content/themes/studiometa/dist/js/config.js
@@ -1,21 +1,2 @@
-(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[2],{
-
-/***/ "./web/wp-content/themes/studiometa/src/js/config.js":
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"isDev\", function() { return isDev; });\n/**\n * Test if the current env is a dev env.\n *\n * @return {Boolean}\n */\nvar isDev = function isDev() {\n  return !window.location.hostname.startsWith('www.');\n};\n/* harmony default export */ __webpack_exports__[\"default\"] = ({\n  isDev: isDev\n});\n\nif (true) {\n  module.hot.accept(function (err) {\n    if (err) {\n      console.error(err);\n    }\n  });\n}//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi93ZWIvd3AtY29udGVudC90aGVtZXMvc3R1ZGlvbWV0YS9zcmMvanMvY29uZmlnLmpzLmpzIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vLy4vd2ViL3dwLWNvbnRlbnQvdGhlbWVzL3N0dWRpb21ldGEvc3JjL2pzL2NvbmZpZy5qcz84ODRlIl0sInNvdXJjZXNDb250ZW50IjpbIi8qKlxuICogVGVzdCBpZiB0aGUgY3VycmVudCBlbnYgaXMgYSBkZXYgZW52LlxuICpcbiAqIEByZXR1cm4ge0Jvb2xlYW59XG4gKi9cbmV4cG9ydCB2YXIgaXNEZXYgPSBmdW5jdGlvbiBpc0RldigpIHtcbiAgcmV0dXJuICF3aW5kb3cubG9jYXRpb24uaG9zdG5hbWUuc3RhcnRzV2l0aCgnd3d3LicpO1xufTtcbmV4cG9ydCBkZWZhdWx0IHtcbiAgaXNEZXY6IGlzRGV2XG59O1xuXG5pZiAobW9kdWxlLmhvdCkge1xuICBtb2R1bGUuaG90LmFjY2VwdChmdW5jdGlvbiAoZXJyKSB7XG4gICAgaWYgKGVycikge1xuICAgICAgY29uc29sZS5lcnJvcihlcnIpO1xuICAgIH1cbiAgfSk7XG59Il0sIm1hcHBpbmdzIjoiQUFBQTtBQUFBO0FBQUE7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0EiLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./web/wp-content/themes/studiometa/src/js/config.js\n");
-
-/***/ }),
-
-/***/ 1:
-/***/ (function(module, exports, __webpack_require__) {
-
-__webpack_require__("./node_modules/webpack/hot/dev-server.js");
-__webpack_require__("./node_modules/webpack-hot-middleware/client.js");
-module.exports = __webpack_require__("./web/wp-content/themes/studiometa/src/js/config.js");
-
-
-/***/ })
-
-},[[1,4,5]]]);
+(self.webpackChunkmodern=self.webpackChunkmodern||[]).push([[723],{646:(n,s,e)=>{e.d(s,{r:()=>o});const o=()=>!window.location.hostname.startsWith("www.")}},n=>{var s;s=646,n(n.s=s)}]);
+//# sourceMappingURL=config.js.map
