2f2211366c023798b388c93aeaa0d5da773cbb22
Titouan Mathis <titouan@studiometa.fr>
2022-06-15 16:45:14 +0200

    Update Tailwind configuration for v3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /**
  * TailwindCSS Configuration File
  *
- * @docs    https://tailwindcss.com/docs/configuration
+ * @see     https://tailwindcss.com/docs/configuration
  * @default https://github.com/studiometa/tailwind-config/blob/develop/src/index.js
  */
 module.exports = {
@@ -9,13 +9,10 @@ module.exports = {
   // Extends the default Studio Meta Tailwind configuration here...
   // plugins: [...],
   // theme: {...},
-  purge: {
-    // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
-    content: [
-      './web/wp-content/themes/studiometa/src/js/**/*.js',
-      './web/wp-content/themes/studiometa/src/js/**/*.vue',
-      './web/wp-content/themes/studiometa/templates/**/*.twig',
-    ],
-  },
-  mode: 'jit',
+  // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
+  content: [
+    './web/wp-content/themes/studiometa/src/js/**/*.js',
+    './web/wp-content/themes/studiometa/src/js/**/*.vue',
+    './web/wp-content/themes/studiometa/templates/**/*.twig',
+  ],
 };
