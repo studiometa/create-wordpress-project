f1ab0826f56424921a1a9039e61b38847777ed3b
Titouan Mathis <titouan@studiometa.fr>
2022-06-24 11:09:36 +0200

    Remove Babel
    It is not used when using the `modern` target in `meta.config.js`.

--- .babelrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  presets: ['@babel/preset-env'],
-  plugins: [
-    '@babel/plugin-transform-runtime',
-    '@babel/plugin-syntax-dynamic-import',
-    '@babel/plugin-proposal-class-properties',
-  ],
-};
